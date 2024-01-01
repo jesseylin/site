cf8ab6a8111ba762fb171167937cc82dd9afacd4
Jesse Y Lin <jesseyounglin@gmail.com>
2024-01-01 00:50:44 -0600

    feat: refactor flake to use direnv

--- flake.nix
+++ flake.nix
@@ -1,16 +1,15 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; };
+  description = "Build for my static site.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = inputs@{ self, nixpkgs, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-linux"
-      ];
-    in {
-      packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in pkgs.callPackage ./site.nix { inherit self; });
-    };
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = pkgs.callPackage ./site.nix { inherit self; };
+        devShells.default = pkgs.mkShell { packages = [ pkgs.hugo ]; };
+      });
 }
